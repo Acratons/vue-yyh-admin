c27bbf201c24e1169cd2403c877f7ac0aa8ebd5c
YYH <15320788989@189.cn>
2023-02-27 15:49:06 +0800

    aside组件栏完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from "../views/Home.vue";
+import Index from '../views/Index.vue'
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/index',
+      name: 'Index',
+      component: () => import('../views/Index.vue')
     }
   ]
 })
